2dbcbc239615901997fbc8c4b5e494ec7a2a2de7
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-23 02:25:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 speakers = ["Edsger","Ada","Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(speakers)
-  return "Hello, my name is #{speakers}."
+def batch_badge_creator(attendees)
+  return "Hello, my name is #{attendees}."
 end
