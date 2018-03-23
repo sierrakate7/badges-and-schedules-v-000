04c24e9fa557e00c00a97e1fc5ba68fa50dc5db8
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-23 02:31:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+
+
+
+def assign_rooms(array)
+  new_array = []
+  counter = 1
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return new_array
+end
+
+
+def printer(array)
+  batch_badge_creator(array).each do |id_badge|
+    puts id_badge
+  end 
+  
+  assign_rooms(array).each do |id_badge|
+    puts id_badge 
+  end
+end 
+
